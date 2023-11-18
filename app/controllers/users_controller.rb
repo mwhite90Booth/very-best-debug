8065c28a8d1611732044c3acc4c796d2bdf5ebe4
mwhite90Booth <146589669+mwhite90Booth@users.noreply.github.com>
2023-11-18 03:45:49 +0000

    fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
     @users = matching_users.order(:created_at)
-
-    render({ :template => "users_templates/all_users"})
+    render({ :template => "user_templates/all_users"})
   end
   
   def show
@@ -30,7 +29,7 @@ class UsersController < ApplicationController
     
     the_user.username = params.fetch("query_username")
     the_user.save
-    redirect_to("/users/#{user.username}")
+    redirect_to("/users/#{the_user.username}")
   end
 
 end
